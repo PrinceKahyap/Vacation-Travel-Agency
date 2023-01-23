ad65f9e1be2363308f21200cb5c636c7eab06289
Princekashyap <vasukashyap08@gmail.com>
2023-01-23 13:13:01 +0530

    i have add responsive design and this project is complete

--- main.js
+++ main.js
@@ -24,3 +24,43 @@ counters.forEach(counter => {
 // footer date 
 const year = document.querySelector("#current-year")
 year.innerHTML = new Date().getFullYear();
+
+// owlCarousel 
+$(".carousel").owlCarousel({
+    margin: 30,
+    loop: true,
+    autoplay: true,
+    autoplayTimeout: 2000,
+    autoplayHoverPause: true,
+    responsive:{
+     0:{
+         items: 1,
+         nav: false
+     },
+     768:{
+         items: 2,
+         nav: false
+     },
+     1000:{
+         items: 3,
+         nav: false
+     },
+    }
+ });
+
+//  hamburger 
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+hamburger.addEventListener("click",mobileMenu);
+function mobileMenu(){
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+}
+const navLink = document.querySelectorAll(".nav-link");
+
+        navLink.forEach(n => n.addEventListener("click", closeMenu));
+
+        function closeMenu() {
+            hamburger.classList.remove("active");
+            navMenu.classList.remove("active");
+        };
